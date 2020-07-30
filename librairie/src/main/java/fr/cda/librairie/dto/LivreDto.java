b1bd238110677a102df499d8e9449800de1ddfb0
matthieu <matthieu_waroux59@hotmail.com>
2020-07-30 17:11:36 +0200

    second commit

--- librairie/src/main/java/fr/cda/librairie/dto/LivreDto.java
+++ librairie/src/main/java/fr/cda/librairie/dto/LivreDto.java
@@ -1,5 +1,16 @@
 package fr.cda.librairie.dto;
 
-public class LivreDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class LivreDto {
+	private int reference;
+	private int prix;
+	private int quantitee;
+	private String titre;
+	private String nbPage;
+	private String auteur;
+	private String editeur;
 }
--- librairie/src/main/java/fr/cda/librairie/entity/Livre.java
+++ librairie/src/main/java/fr/cda/librairie/entity/Livre.java
@@ -1,6 +1,31 @@
 package fr.cda.librairie.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Livre {
 
+	@Id
+	private int reference;
+	private int prix;
+	private int quantitee;
 	private String titre;
+	@Column(name = "nombre_page")
+	private String nbPage;
+
+	@ManyToOne
+	Auteur auteur;
+
+	@ManyToOne
+	Editeur editeur;
 }
