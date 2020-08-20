72461db36269fbe84c4042298b51746de40904f7
Fethi Benseddik <fe.benseddik@gmail.com>
2020-08-20 09:24:09 +0200

    gestion conflits

--- librairie/src/main/java/fr/cda/librairie/controller/AccueilController.java
+++ librairie/src/main/java/fr/cda/librairie/controller/AccueilController.java
@@ -6,16 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccueilController {
 
-<<<<<<< HEAD
-    @GetMapping(value = "/accueil")
-    public String m2() {
-        return "index";
-    }
-
-=======
 	@GetMapping(value = "/index")
 	public void sayHello() {
 	
 	}
->>>>>>> Dev
 }
