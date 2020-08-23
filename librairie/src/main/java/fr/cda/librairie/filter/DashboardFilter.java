32f8f50a526d979d6391b3662249e76982435909
Fethi Benseddik <fe.benseddik@gmail.com>
2020-08-23 20:03:08 +0200

    filtre dashboard

--- librairie/src/main/java/fr/cda/librairie/filter/DashboardFilter.java
+++ librairie/src/main/java/fr/cda/librairie/filter/DashboardFilter.java
@@ -1,36 +1,37 @@
-//package fr.cda.librairie.filter;
-//
-//import fr.cda.librairie.dto.UtilisateurDto;
-//import fr.cda.librairie.utils.Constantes;
-//
-//import javax.servlet.*;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@WebFilter("/dashboard")
-//public class DashboardFilter implements Filter {
-//
-//
-//    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-//
-//        System.out.println("\n--------->>> filtre authentification");
-//        HttpServletRequest reqH = (HttpServletRequest) req;
-//
-//        UtilisateurDto currentUser = (UtilisateurDto) reqH.getSession().getAttribute("user");
-//
-//        if (currentUser.getLabelRole().equals("Client")|| currentUser == null) {
-//            ((HttpServletResponse) resp).sendRedirect("index");
-//            return;
-//        } else {
-//            chain.doFilter(req, resp);
-//        }
-//
-//    }
-//
-//    public void init(FilterConfig config) throws ServletException {
-//
-//    }
-//
-//}
+package fr.cda.librairie.filter;
+
+import fr.cda.librairie.dto.UtilisateurDto;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/dashboard")
+public class DashboardFilter implements Filter {
+
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        System.out.println("\n--------->>> filtre authentification");
+        HttpServletRequest reqH = (HttpServletRequest) req;
+        UtilisateurDto currentUser = (UtilisateurDto) reqH.getSession().getAttribute("user");
+        if (currentUser == null) {
+            ((HttpServletResponse) resp).sendRedirect("index");
+            return;
+        }
+        if (currentUser.getLabelRole().equals("Client")) {
+            ((HttpServletResponse) resp).sendRedirect("index");
+            return;
+        } else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
